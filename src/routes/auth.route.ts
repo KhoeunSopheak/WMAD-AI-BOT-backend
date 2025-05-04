684e6891806c18f66d3a1d61a2e0d2a8600a08da
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2025-05-05 06:36:40 +0700

    add email validation

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { login, register } from "../controllers/authController";
-import { validateLogin, validateUser } from "../midleware/validationMidleware";
+import { login, register } from "../controllers/auth.controller";
+import { validateLogin, validateUser } from "../midleware/validation.midleware";
 
 
 const router = Router();
