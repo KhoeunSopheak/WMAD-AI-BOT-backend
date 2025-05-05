6f10e822de2dc7e4a1e3b547ef6ad21832317774
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2025-05-05 09:22:02 +0700

    update validation

--- src/routes/teacher.route.ts
+++ src/routes/teacher.route.ts
@@ -8,13 +8,14 @@ import {
 } from "../controllers/teacher.controller";
 import protectRoute from "../midleware/auth.midleware";
 import { RoleEnum } from "../common";
+import { validateTeacher } from "../midleware/validation.midleware";
 
 const router = Router();
 
-router.post("/", protectRoute([RoleEnum[0]]), createTeacher);
-router.get("/", protectRoute([RoleEnum[0]]), getAllTeachers);
-router.get("/:id",protectRoute([RoleEnum[0]]), getTeacherById);
-router.put("/:id",protectRoute([RoleEnum[0]]),  updateTeacher);
-router.delete("/:id",protectRoute([RoleEnum[0]]), deleteTeacher);
+router.post("/", protectRoute([RoleEnum[0]]), validateTeacher, createTeacher);
+router.get("/", protectRoute([RoleEnum[0]]), validateTeacher, getAllTeachers);
+router.get("/:id",protectRoute([RoleEnum[0]]), validateTeacher, getTeacherById);
+router.put("/:id",protectRoute([RoleEnum[0]]), validateTeacher, updateTeacher);
+router.delete("/:id",protectRoute([RoleEnum[0]]), validateTeacher, deleteTeacher);
 
 export default router;
