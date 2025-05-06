71eab41b71e817afcaa1310eb470922734454965
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2025-05-06 13:57:00 +0700

    roadmap feature

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import authRouter from "./src/routes/auth.route";
 import studentRoute from './src/routes/students.route';
 import quizzRoute from './src/routes/quizzes.route';
 import chatRoute from './src/routes/chat.route';
+import roadmapRoute from './src/routes/roadmap.route';
 
 const app = express();
 const PORT = 3003;
@@ -13,8 +14,9 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRouter);
 app.use("/api/users/students", studentRoute);
-app.use("/api/users/quizzes", quizzRoute)
-app.use('/api/chat', chatRoute)
+app.use("/api/users/quizzes", quizzRoute);
+app.use('/api/users/chats', chatRoute);
+app.use("/api/users/roadmaps", roadmapRoute);
 
 // Start server
 app.listen(PORT, () => {
